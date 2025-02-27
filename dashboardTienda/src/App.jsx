655d0c64511af58ff32447c95abc6fc583fcd4a3
julian19G <julian19guerra@gmail.com>
2025-02-27 14:17:17 -0500

    Primeras implementaciones

--- dashboardTienda/src/App.jsx
+++ dashboardTienda/src/App.jsx
@@ -1,21 +1,19 @@
 import { useState } from 'react'
 import './App.css'
+import Sidebar from './Sidebar'
+import Header from './Header'
+import Home from './Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-     
-
-      </div>
+    <div className='grid-container'>
+    <Header />
+    <Sidebar />
+    <Home />
+    </div>
     </>
   )
 }
